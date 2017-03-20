8f64612b3a71476e185b31961969982046238837
UserPL135Pc19 <UserPL135Pc19@SLFRPL135PC19.ensino.atec.local>
2017-03-20 18:50:28 +0000

    fase 3

--- src/controller/IntroCliente.java
+++ src/controller/IntroCliente.java
@@ -1,5 +1,47 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.Cliente;
+
+
 public class IntroCliente {
+
+	ArrayList<Cliente> iCliente;
+	
+public IntroCliente() {
+	
+	this.iCliente = new ArrayList<Cliente>();	
+	}
+
+public void Cliente(String nome, String morada, String email, String telefone) {
+	
+	Cliente c = new Cliente(nome, morada, email, telefone);
+	int novoId = iCliente.size();
+	
+	c.setId(novoId);
+	
+	iCliente.add(c);	
+}
+
+public void iCliente(Cliente c) {
+	int novoId = iCliente.size();
+	
+	c.setId(novoId);
+	
+	iCliente.add(c);
+}
+
+public Cliente getCliente(int id) {
+
+	for (Cliente c : iCliente) {
+		
+		if (c.getId()==id)
+			return c;
+	}
+	return null;
+}
+
+
 
 }
--- src/controller/IntroRestaurante.java
+++ src/controller/IntroRestaurante.java
@@ -1,5 +1,46 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.Restaurante;
+
 public class IntroRestaurante {
 
-}
+	ArrayList<Restaurtante> iRestaurante;
+	
+	public IntroRestaurante() {
+		
+		this.iRestaurante = new ArrayList<Restaurante>();	
+		}
+
+	public void Restaurante(String nome, String morada, String contacto, String especialidade) {
+		
+		Restaurante r = new Restaurante(nome, morada, contacto, especialidade);
+		int novoId = iRestaurante.size();
+		
+		r.setId(novoId);
+		
+		iRestaurante.add(r);	
+	}
+
+	public void iRestaurante(Restaurante r) {
+		int novoId = iRestaurante.size();
+		
+		r.setId(novoId);
+		
+		iRestaurante.add(r);
+	}
+
+	public Restaurante getRestaurante(int id) {
+
+		for (Restaurante r : iRestaurante) {
+			
+			if (r.getId()==id)
+				return r;
+		}
+		return null;
+	}
+
+
+
+	}
