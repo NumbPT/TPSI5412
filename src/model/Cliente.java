df064e69a9725af473cdf8b48c38636e6c7fcd23
UserPL135Pc19 <UserPL135Pc19@SLFRPL135PC19.ensino.atec.local>
2017-03-20 18:03:19 +0000

    1 passo

---
+++ src/model/Cliente.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Cliente {
+
+	private String nome;
+	private String morada;
+	private String email;
+	private String telefone;
+	
+	int id;
+}
